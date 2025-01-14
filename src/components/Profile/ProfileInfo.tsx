5fdee39e6379d7bad46d047d672da46782b68a6a
mananjot-17 <mananjot.singh@aimlarger.com>
2025-01-14 13:14:04 +0530

    added typewrite

--- src/components/Profile/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo.tsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-
+import { Typewriter } from 'react-simple-typewriter';
 
 const ProfileInfo = () => (
   <motion.div
@@ -8,16 +8,20 @@ const ProfileInfo = () => (
     transition={{ duration: 0.5, delay: 0.2 }}
     className="text-center md:text-left"
   >
-    <h1 className="text-4xl md:text-5xl font-semibold  mb-2">
-      Mananjot Singh
-    </h1>
+    <h1 className="text-4xl md:text-5xl font-semibold mb-2">Mananjot Singh</h1>
+
     <h2 className="text-xl md:text-2xl text-blue-600 mb-4">
-      Full Stack Developer
+      <Typewriter
+        words={['Full Stack Developer']}
+        loop={false} // Only type it once
+        cursor
+        cursorStyle="_"
+        typeSpeed={100}
+        deleteSpeed={50}
+        delaySpeed={1000}
+      />
     </h2>
-    {/* <div className="flex items-center justify-center md:justify-start text-gray-600 mb-6">
-      <MapPin size={20} className="mr-2" />
-      <span>San Francisco, CA</span>
-    </div> */}
+
     <p className="text-gray-600 max-w-lg mb-8">
       Passionate full-stack developer with expertise in the MERN stack. 
       I create scalable web applications with modern technologies and best practices.
